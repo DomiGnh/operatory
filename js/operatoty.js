b7916fd95c2393fdec18b2deb2195be48337d97f
Dominika Gnich <d.gnich@wp.pl>
2017-06-10 14:17:04 +0200

    operatory

---
+++ js/operatoty.js
@@ -0,0 +1,8 @@
+'use strict';
+//mnożen ie, dzielenie, dodawanie, odejmowanie, dzielenie modulo, inkrementacja czyli zwięszanie, dekrementacja czyli zmiejszenie//
+
+//operatory przypisania- przypisujemy wartość//
+
+var licz = 2;
+licz += 3; //licz= licz +3
+console.log(licz);
