3c13230b1bcda39c732f6d6cc16b7d373b534f0a
Dominika Gnich <d.gnich@wp.pl>
2017-06-10 14:28:26 +0200

    comit

--- js/operatoty.js
+++ js/operatoty.js
@@ -6,3 +6,29 @@
 var licz = 2;
 licz += 3; //licz= licz +3
 console.log(licz);
+
+//operatoty porónania stsouje sie w warunkach if, while itp.//
+
+
+var liczba1= "13", liczba2 = 13;
+
+if (liczba1 == liczba2){
+    console.log("równe");
+}
+
+//operatory logiczne and, or, not//
+
+var liczba1 = 13, liczba2 = 5;
+
+if ((liczba2 > 3) && (liczba2 < 10)){
+    console.log("tak");
+}
+
+
+
+
+
+
+
+
+
